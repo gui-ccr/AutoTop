3c53f6430aa40d44d5623a5c7deef79e3d0ea260
Guilherme <116519824+gui-ccr@users.noreply.github.com>
2025-10-31 18:05:15 -0300

    otimização para deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/concessionaria-site/", // <-- ADICIONE ESTA LINHA
+  base: "/AutoTop/", // <-- ADICIONE ESTA LINHA
 })
